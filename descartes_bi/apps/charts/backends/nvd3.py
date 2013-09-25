29385f7d4e72f1f4988ce5d2cce256c663b05f19
Sasha Méndez <sasha.mendez@gmail.com>
2013-09-25 12:04:26 -0400

    Incorporated changes from branch chart_templates. Multibarchart example only.

--- descartes_bi/apps/charts/backends/nvd3.py
+++ descartes_bi/apps/charts/backends/nvd3.py
@@ -27,11 +27,22 @@ class NovusD3(ChartBackend):
 
     def render(self, report, request):
         series_results = report.execute(request)
+
+        serie = []
+
+        #TODO: Improve
+        for series_result in series_results:
+            for i in series_result:
+                for k, v in i.iteritems():
+                    serie.append({"x": k, "y": v})
+
+        series_chart_data = [{"values": serie, "key": report.title, "bar": "true"}]
+
         context = {
-            'series_results': [json.dumps(result) for result in series_results],
+            'series_results': """%s;\n""" % json.dumps(series_chart_data),
             'report': report,
-            'chart_type': 'LI',
+            'chart_type': 'SI',
         }
 
-        return render_to_response('charts/novus/merged.html', context,
+        return render_to_response('charts/novus/multiBarChart.html', context,
             context_instance=RequestContext(request))
